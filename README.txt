3f346286efd28434066083e2ea1cacf807a94914
Igal Koshevoy <igal@pragmaticraft.com>
2008-07-03 11:24:25 -0700

    Added -h option and moved instructions to README.txt

---
+++ README.txt
@@ -0,0 +1,43 @@
+rubychecker - runs checks on a Ruby interpreter
+
+USAGE: rubychecker.sh [OPTIONS]
+
+OPTIONS:
+    -t tag
+        Name of interpreter to use when naming reports, e.g., "p111" or
+        "svn12345". Defaults to "current".
+
+    -v
+        Displays version number and quits.
+
+    -C
+        Skip checkouts
+
+    -S
+        Skip all preparations that can be skipped
+
+EXAMPLE:
+    bash rubychecker.sh
+        Runs test suites against the "ruby" interpreter in your current shell
+        and records them to the "reports" directory.
+
+DEPENDENCIES:
+    - ruby
+    - bash
+    - grep
+    - wget
+    - git
+    - svn
+    - tar
+    - MySQL headers
+    - SQLite3 headers
+    - MySQL server
+        You'll need to login as DBA and run:
+            CREATE DATABASE activerecord_unittest;
+            CREATE DATABASE activerecord_unittest2;
+            CREATE DATABASE actionwebservice_unittest;
+            CREATE USER 'rails'@'localhost' IDENTIFIED BY PASSWORD '';
+            GRANT ALL ON activerecord_unittest.* TO 'rails'@'localhost';
+            GRANT ALL ON activerecord_unittest2.* TO 'rails'@'localhost';
+            GRANT ALL ON actionwebservice_unittest.* TO 'rails'@'localhost';
+            FLUSH PRIVILEGES;
--- rubychecker.sh
+++ rubychecker.sh
@@ -1,57 +1,13 @@
 #!/bin/bash
 
-# Please run this program with "--help" or review the "usage" function
-# below for information on this program and how to use it.
+# Please run this program with "-h" or see the "README.txt"  for 
+# information on this program and how to use it.
 
 #=======================================================================
 
 # Display usage
 usage () {
-    cat <<HERE
-rubychecker - runs checks on a Ruby interpreter
-
-USAGE: rubychecker.sh [OPTIONS]
-
-OPTIONS:
-    -t tag
-        Name of interpreter to use when naming reports, e.g., "p111" or
-        "svn12345". Defaults to "current".
-
-    -v
-        Displays version number and quits.
-
-    -C
-        Skip checkouts
-
-    -S
-        Skip all preparations that can be skipped
-
-EXAMPLE:
-    bash rubychecker.sh
-        Runs test suites against the "ruby" interpreter in your current shell
-        and records them to the "reports" directory.
-
-DEPENDENCIES:
-    - ruby
-    - bash
-    - grep
-    - wget
-    - git
-    - svn
-    - tar
-    - MySQL headers
-    - SQLite3 headers
-    - MySQL server
-        You'll need to login as DBA and run:
-            CREATE DATABASE activerecord_unittest;
-            CREATE DATABASE activerecord_unittest2;
-            CREATE DATABASE actionwebservice_unittest;
-            CREATE USER 'rails'@'localhost' IDENTIFIED BY PASSWORD '';
-            GRANT ALL ON activerecord_unittest.* TO 'rails'@'localhost';
-            GRANT ALL ON activerecord_unittest2.* TO 'rails'@'localhost';
-            GRANT ALL ON actionwebservice_unittest.* TO 'rails'@'localhost';
-            FLUSH PRIVILEGES;
-HERE
+    cat $(dirname $0)"/README.txt"
 }
 
 #===[ Settings ]========================================================
@@ -220,9 +176,13 @@ TAG="current"
 SKIP_CHECKOUTS=0
 
 # Process arguments
-while getopts 't:vGRSC' OPTION
+while getopts 'ht:vGRSC' OPTION
 do
     case $OPTION in
+    h)
+        usage
+        exit 0
+        ;;
     t)
         TAG="$OPTARG"
         ;;
