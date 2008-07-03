08533e47c43a5565415d4b48d5c1abce4dac51ac
Igal Koshevoy <igal@pragmaticraft.com>
2008-07-03 11:21:10 -0700

    Added documentation for examples and dependencies.

--- rubychecker.sh
+++ rubychecker.sh
@@ -25,6 +25,32 @@ OPTIONS:
 
     -S
         Skip all preparations that can be skipped
+
+EXAMPLE:
+    bash rubychecker.sh
+        Runs test suites against the "ruby" interpreter in your current shell
+        and records them to the "reports" directory.
+
+DEPENDENCIES:
+    - ruby
+    - bash
+    - grep
+    - wget
+    - git
+    - svn
+    - tar
+    - MySQL headers
+    - SQLite3 headers
+    - MySQL server
+        You'll need to login as DBA and run:
+            CREATE DATABASE activerecord_unittest;
+            CREATE DATABASE activerecord_unittest2;
+            CREATE DATABASE actionwebservice_unittest;
+            CREATE USER 'rails'@'localhost' IDENTIFIED BY PASSWORD '';
+            GRANT ALL ON activerecord_unittest.* TO 'rails'@'localhost';
+            GRANT ALL ON activerecord_unittest2.* TO 'rails'@'localhost';
+            GRANT ALL ON actionwebservice_unittest.* TO 'rails'@'localhost';
+            FLUSH PRIVILEGES;
 HERE
 }
 
